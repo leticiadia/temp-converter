6cdc35bbb54c3ded46b9cb043a2afd7f6e98fc40
Leticia Dias <contatoleticiadia@gmail.com>
2021-03-23 19:39:51 -0300

    file

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function conversorTemperatura(){
 
+    const form = document.querySelector('form')
+    form.addEventListener('submit', e => e.preventDefault())
+
     var temperatura = document.getElementById('conversor').value
-
     tempo = parseFloat((temperatura * 9 / 5) + 32)
-
-    document.getElementById('info').innerHTML = "Temperatura em Graus Fahrenheit " + tempo + " ºF";
+    document.getElementById('info').innerHTML = "Temperatura em Graus Fahrenheit " + tempo + " ºF"
 
 }
 
 
-console.log(conversorTemperatura())
+conversorTemperatura()
 
 
 
