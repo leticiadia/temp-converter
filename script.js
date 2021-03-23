7c73f4a214f432b825bfd121c2a6d145bccc1291
Leticia Dias <contatoleticiadia@gmail.com>
2021-03-23 20:00:52 -0300

    "file"

--- script.js
+++ script.js
@@ -11,8 +11,3 @@ function conversorTemperatura(){
 
 
 conversorTemperatura()
-
-
-
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
